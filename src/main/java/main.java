e4ac58eab9e4cceef898657bd1a84225859b5605
Kam That2 <kamthat2@Kameshs-MacBook-Pro.local>
2023-03-04 11:55:59 -0800

    First commit

---
+++ src/main/java/main.java
@@ -0,0 +1,10 @@
+public class main {
+    public static void main(String[] args){
+
+
+        Product product = new Product("Coke", 20.43);
+
+        System.out.printf("Hello World 2023!!");
+        System.out.printf("name: %s, price: %f", product.getName(), product.getPrice());
+    }
+}
